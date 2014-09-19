876b2d49796cae38f75ff8a50e375ba2189d14cb
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2014-09-18 18:51:40 -0700

    Add menu visuals

--- js/app/interface.js
+++ js/app/interface.js
@@ -1,7 +1,9 @@
 define([],
 function () {
 
-  var MODAL = {
+  var MODAL;
+
+  MODAL = {
 
     hide: function(){
       $('#fader, #intro_wrapper').fadeOut(200)
@@ -11,26 +13,42 @@ function () {
     show: function(){
       $('#fader, #intro_wrapper').fadeIn(200)
       $('#toggle_intro').addClass('active');
+    },
+
+    open: function(){
+      return !!$('#toggle_intro.active').length;
     }
 
   }
 
+  // cannot bind within object definition.
+  MODAL['toggle'] = function(){
+    if (this.open()){
+      this.hide();
+    }else {
+      this.show();
+    }
+  }.bind(MODAL);
+
+  $(document).on('keydown', function(e){
+    if (e.keyCode === 27 && MODAL.open()) {
+      MODAL.hide()
+    }
+  });
+
   $(function(){
 
-    $('#fader, #begin_button').click(function(){
-      MODAL.hide();
+    $('#fader, #begin_button').click(MODAL.hide);
+
+    $('#toggle_intro').click(function(e){
+      e.preventDefault();
+      MODAL.toggle()
     });
 
-    $('#toggle_intro').click(function(){
-
-      if ($('#toggle_intro.active').length){
-        MODAL.hide();
-      } else {
-        MODAL.show();
-      }
-
-    })
-
+    $('#reed_banks_menu_control').click(function(e){
+      e.preventDefault();
+      $('#reed_banks_menu').toggle();
+    });
 
   });
 
